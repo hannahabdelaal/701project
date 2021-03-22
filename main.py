5db71cc53b947c43819e0a63a196acb058e90a3b
habde <habdelaal@middlebury.edu>
2021-03-22 05:11:32 +0000

    pyarabic practice

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from pyarabic.araby import strip_tashkeel
+from pyarabic.araby import strip_harakat
 
 if __name__ == '__main__':
+  text = u"الْعَرَبِيّةُ"
+  print(strip_harakat(text))
   text = u"الْعَرَبِيّةُ"
   print(strip_tashkeel(text))
